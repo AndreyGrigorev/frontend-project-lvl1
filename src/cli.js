bfa055005272accd79e9984366288272354a1c7a
andreygrigorev <grigorevandreyzool@gmail.com>
2022-02-12 19:06:23 +0400

    eslint

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+function namePlayer() {
+	const name = readlineSync.question('May I have your name? ');
+	console.log('Hello, ' + name +'!');
+
+	return;
+}
+
+export {namePlayer};
